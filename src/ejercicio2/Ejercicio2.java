e2ea6537bd2c93be76a8f655ea19f20416a0ddf7
luis <lagonzalez1688@gmail.com>
2020-09-30 00:55:29 -0700

    Ejercicio de suma

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2;
+
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+    int a=30;
+    int b=50;
+    int suma=0;
+    
+        suma= a + b;
+        System.out.print(" el resultado de la suma es: " +suma);
+        
+    }
+    
+}
+
